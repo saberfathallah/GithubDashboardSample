70cefe00b6c6618252c5ed8f984b7c7bda98310e
saber <saberfathallah7@gmail.com>
2019-05-19 00:01:57 +0200

    add searchUser function

---
+++ githubdashboardsample/src/Components/UserSearch/actions.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+import { get } from 'lodash';
+import { SEARCH_USER } from './constants';
+
+const apiUrl = 'https://api.github.com/users/';
+
+export const searchUserSuccess = (user, repos) => {
+  return {
+    type : SEARCH_USER,
+    payload :{
+      user,
+      repos,
+    }
+  }
+}
+
+export const searchUser = (login) => {
+  return(dispatch) => {
+     axios.get(`${apiUrl}${login}`)
+    .then( async(response) => {
+      const data = await axios.get(response.data.repos_url);
+      const repos = get(data, 'data', []);
+      dispatch(searchUserSuccess(response.data, repos));
+    })
+    .catch(error=> { 
+      throw(error);
+    });
+  };
+}
