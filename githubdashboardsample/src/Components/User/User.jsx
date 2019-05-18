828df5d11bd3605756632d218fc08aa9c7d67066
saber <saberfathallah7@gmail.com>
2019-05-19 01:02:35 +0200

    redirect to repoDetails

--- githubdashboardsample/src/Components/User/User.jsx
+++ githubdashboardsample/src/Components/User/User.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
+import { compose } from 'recompose';
 import { map } from 'lodash';
 import userWrapper from './UserWrapper';
 
-const User = ({ user, className, repos }) => {
-  const reposUser = map(repos, (rep) => (
-    <div key={rep.id} className="user-repos">
+const User = ({ user, className, repos, history }) => {
+  const reposUser = map(repos, (rep, index) => (
+    <div onClick={() => history.push(`/RepoDetails/${index}`)} key={rep.id} className="user-repos">
       <p className="user-repos_icon">....</p>
       <p key={rep.id}>{rep.name}</p>
     </div>
@@ -25,6 +27,7 @@ User.propTypes = {
   user: PropTypes.object,
   className: PropTypes.string,
   repos: PropTypes.array,
+  history: PropTypes.object,
 };
 
-export default userWrapper(User);
+export default compose(withRouter, userWrapper)(User);
