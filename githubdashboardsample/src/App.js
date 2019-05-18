0aed12b551a9cff473333390763710e4e92a6008
saber <saberfathallah7@gmail.com>
2019-05-19 00:59:02 +0200

    fix routes

--- githubdashboardsample/src/App.js
+++ githubdashboardsample/src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import { Route, Switch, withRouter } from 'react-router-dom';
 import './App.css';
 import Header from './Components/Header';
 import UserSearch from './Components/UserSearch';
+import RepoDetails from './Components/RepoDetails'
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <UserSearch />
+      <Switch> 
+        <Route exact path='/' component={UserSearch}/>
+        <Route exact path='/RepoDetails/:index' component={withRouter(RepoDetails)}/>
+      </Switch> 
     </div>
   );
 }
