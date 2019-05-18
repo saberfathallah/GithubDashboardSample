44aa8cd1100fe243a2f69d8aeba5e51da0604c82
saber <saberfathallah7@gmail.com>
2019-05-18 14:50:18 +0200

    add CSS UserSearch component

---
+++ githubdashboardsample/src/Components/UserSearch/UserSearchWrapper.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export default Component => styled(Component)`
+  .userSearch-input_search {
+    border: none;
+    height: 30px;
+  }
+  .userSearch-button_search {
+    height: 30px;
+    color: white;
+    border: none;
+    background: #f36799;
+    width: 85px;
+  }
+`;
