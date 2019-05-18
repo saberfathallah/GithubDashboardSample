aeed2ced99b66870d4be1a1f4be6ed781ed79b55
saber <saberfathallah7@gmail.com>
2019-05-18 22:42:57 +0200

    fix CSS user  component

---
+++ githubdashboardsample/src/Components/User/UserWrapper.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export default Component => styled(Component)`
+  .user-block {
+     display: flex;
+  }
+  .user-block-img {
+    width: 50px;
+    height: 50px;
+  }
+`;
