8bf1e10b5fee1e1b4f4309af3b93d47321876e5b
saber <saberfathallah7@gmail.com>
2019-05-18 14:04:44 +0200

    add CSS component Header

---
+++ githubdashboardsample/src/Components/HeaderWrapper.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export default Component => styled(Component)`
+  height: 60px;
+  color: white;
+  background: #08d4f7;
+  margin-bottom: 18px;
+  text-align: center;
+  
+  .header-title {
+    font-size: 24px;
+  }
+`;
