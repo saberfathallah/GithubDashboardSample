a4eb1fa6f91a3e68aef7f56d74e3bcdb24ab4aea
saber <saberfathallah7@gmail.com>
2019-05-19 00:03:02 +0200

    fix userSearch

--- githubdashboardsample/src/Components/UserSearch/UserSearch.jsx
+++ githubdashboardsample/src/Components/UserSearch/UserSearch.jsx
@@ -1,19 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { compose } from 'recompose';
 import userSearchWrapper from './UserSearchWrapper';
+import User from '../User';
+import { searchUser } from './actions';
 
-const UserSearch = ({ className }) => {
-  return(
-    <div className={className}>
-      <div>
-        <input
-          placeholder="Username"
-          className="userSearch-input_search"
-          type="search"
-        />
-        <button className="userSearch-button_search">SEARCH</button>
+class UserSearch extends Component {
+
+  handleChange = async (event) => {
+    const { searchUser } = this.props;
+    if (event.target.value) {
+      await searchUser(event.target.value);
+    }
+  }
+
+  render() {
+    const { className, user, repos } = this.props;
+    return(
+      <div className={className}>
+        <div className="user-search">
+          <input
+            placeholder="Username"
+            className="userSearch-input_search"
+            type="search"
+            onChange={(e) => this.handleChange(e)}
+          />
+          <button className="userSearch-button_search">SEARCH</button>
+        </div>
+        {user.id && <User user={user} repos={repos} />}
       </div>
-    </div>
-  )
+    );
+  }
 }
 
-export default userSearchWrapper(UserSearch);
+const mapStateToProps = (state) => {
+  return {
+    user: state.userReducer.user,
+    repos: state.userReducer.repos,
+  };
+}
+
+const mapDispatchToProps = (dispatch) => ({
+  searchUser: (login) => {
+    dispatch(searchUser(login));
+  },
+});
+
+UserSearch.propTypes = {
+  className: PropTypes.string,
+};
+
+export default compose(connect(mapStateToProps,mapDispatchToProps), userSearchWrapper)(UserSearch);
