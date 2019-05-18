a3565ad3316d3b141a08fcebec97a28ba9764cf4
saber <saberfathallah7@gmail.com>
2019-05-19 00:59:17 +0200

    fix routes

--- githubdashboardsample/src/index.js
+++ githubdashboardsample/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
@@ -12,7 +13,9 @@ const store = createStore(rootReducer, applyMiddleware(thunk));
 
 ReactDOM.render( 
   <Provider store={store}>
+    <BrowserRouter>
     <App />
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root')
 );
